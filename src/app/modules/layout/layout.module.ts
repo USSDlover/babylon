959c5eb93bb2cc99ee5e49dbaa4cf9737049014c
Alireza Razinejad <a.razinejad@gmail.com>
2021-12-25 15:23:04 +0330

    Import material and add layout component

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavigationComponent} from './navigation/navigation.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { LayoutComponent } from './layout/layout.component';
 
-
+const AngularMaterial = [
+  MatButtonModule,
+  MatMenuModule
+];
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    LayoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ...AngularMaterial
   ],
   exports: [
     NavigationComponent
